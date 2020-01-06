1386397d6d2339a9de55171a1298d014f6d8f6ec
emard <vordah@gmail.com>
2020-01-06 12:13:32 +0100

    main.py example how to start it

--- esp32/main.py
+++ esp32/main.py
@@ -2,6 +2,9 @@ import network
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
 sta_if.connect("accesspoint", "password")
+#from os import mount
+#from machine import SDCard
+#mount(SDCard(slot=3),"/sd")
 import ecp5
 ecp5.prog("apple2.bit.gz")
 import disk2
